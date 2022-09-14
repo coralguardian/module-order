f24c234234e4a35bb02278a40dbb9c6e74a2547f
d4rk0s <gregory@d4rk0s.net>
2022-09-14 15:00:09 +0200

    Mise en place du nouveau tunnel de paiement

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,7 +2,12 @@
 
 namespace D4rk0snet\CoralOrder;
 
+use D4rk0snet\CoralOrder\API\CreateOrder;
+
 class Plugin
 {
-
+    public static function launchActions()
+    {
+        do_action(\Hyperion\RestAPI\Plugin::ADD_API_ENDPOINT_ACTION, new CreateOrder());
+    }
 }
